f75eb20facd11f7b3c6443210e675771b754e983
Kirubakaran Periyasamy <88470706+kiruba11k@users.noreply.github.com>
2026-02-02 17:10:03 +0530

    Update fetchSegmentRecords.js

--- services/fetchSegmentRecords.js
+++ services/fetchSegmentRecords.js
@@ -1,36 +1,30 @@
 import { getHubspotClient } from "../config/hubspot.js";
 import { normalizeRecords } from "../utils/normalize.js";
 
+const MAX_SAMPLE = 200;
+
 export async function fetchSegmentRecords({
   accessToken,
   mode,
   listId,
   pipelineId,
   stages,
-  limit = 200
+  limit = MAX_SAMPLE
 }) {
   const hubspot = getHubspotClient(accessToken);
+  const safeLimit = Math.min(limit, MAX_SAMPLE);
 
   let records = [];
 
-  if (mode === "list") {
-    const res = await hubspot.apiRequest({
-      method: "GET",
-      path: `/crm/v3/lists/${listId}/memberships/joined`,
-      qs: { limit }
-    });
-    records = res.body.results;
-  }
-
   if (mode === "deals") {
     const res = await hubspot.crm.deals.searchApi.doSearch({
+      limit: safeLimit,
       filterGroups: [{
         filters: [
           { propertyName: "pipeline", operator: "EQ", value: pipelineId },
           { propertyName: "dealstage", operator: "IN", values: stages }
         ]
-      }],
-      limit
+      }]
     });
     records = res.results;
   }
